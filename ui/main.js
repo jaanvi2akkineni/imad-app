e50aa2a6e0680f1164c10cba24057876ca17d98b
jaanvi2akkineni <30662407+jaanvi2akkineni@users.noreply.github.com>
2017-08-26 22:21:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,9 +30,36 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
+    
+     //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+             var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+        
+    }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+        }
+        //not done yet
+    
+    }
+     //make the request
+    request.open('GET', 'http://jaanvi2akkineni.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+    };
+    
+  
     //make a request to the server and send the name
     //capture a list of nmaes and render it as list
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0; i< names.length; i++) {
         list += '<li>' + names[i] + '</li>';

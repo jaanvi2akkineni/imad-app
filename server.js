d6145a5cd269229b0daeb2cfd3e56aba897c76bf
jaanvi2akkineni <30662407+jaanvi2akkineni@users.noreply.github.com>
2017-08-26 22:09:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,16 @@ app.get('/counter', function (req, res){
     res.send(counter.toString());
 });
 
+var names= [];
+app.get('/submit-name/', function(req, res) {
+    //get the name from the request
+    var name = req.query.name;
+    names.push(name);
+    //json: javascript object notation
+    res.send(JSON.stringify(names));//1000
+});
+
+
 app.get('/:articleName', function(req,res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
@@ -110,14 +120,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var names= [];
-app.get('/submit-name/', function(req, res) {
-    //get the name from the request
-    var name = req.query.name;
-    names.push(name);
-    //json: javascript object notation
-    res.send(JSON.stringify(names));//1000
 });
 
 

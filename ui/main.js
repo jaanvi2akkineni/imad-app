9853bacf11972a2d4666f88e6cdb420e2521486b
jaanvi2akkineni <30662407+jaanvi2akkineni@users.noreply.github.com>
2017-08-26 21:41:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onClick = function () {
             if (request.status === 200) {
             var counter = request.responseText;
              var span = document.getElementById('count');
-            span.innerHtml = counter.toString();
+            span.innerHTML = counter.toString();
     
         }
         //not done yet
@@ -24,4 +24,20 @@ button.onClick = function () {
     //make the request
     request.open('GET', 'http://jaanvi2akkineni.imad.hasura-app.io/counter', true);
     request.send(null);
+};
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //make a request to the server and send the name
+    //capture a list of nmaes and render it as list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++) {
+        list <- '<li>' + names[i] + '</li>';
+        
+    }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };
